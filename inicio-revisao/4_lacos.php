c3c7b867ce98612c935fa01cd7604e2b1cd7dc67
BrendonBezerra <bezerrabrendon1@gmail.com>
2026-08-17 11:56:20 -0300

    Update 4_lacos.php

---
+++ inicio-revisao/4_lacos.php
@@ -0,0 +1,29 @@
+<?php
+
+// Laço FOR - Exemplos aplicados em tabuada
+for ($i = 1; $i <=10; $i++) {
+    echo "8 x $i = " . (8 * $i) . "<br>";
+}
+
+
+
+
+
+//While - (Enquanto) Contagem regressiva
+echo "<br>";
+$n = 8;
+while ($n > 0) {
+    echo $n . "<br>";
+    $n--;
+}
+
+
+
+// Do While - (Faça Enquanto) Executa ao menos 1 vez
+
+echo "<br>";
+$j = 0;
+do {
+    echo "j vale: $j <br>";
+    $j++;
+} while ($j <= 10);
